87eb15c9ce2ea5c50f6837bed3059dfcc6897ab1
Stephen <swrobbin@gmail.com>
2021-07-14 10:44:27 -0400

    commit tree 003c41eb94ea5d99bb092d140101a5ac8fd7f276

--- index.js
+++ index.js
@@ -1,6 +1,68 @@
 // Your code here
-function createEmployeeRecord(arr){
-    let newArray = [];
-    newArray[0] = arr.firstname
+let createEmployeeRecord = function(arr){
+    return { 
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
 };
 
+let  createEmployeeRecords = function(arr){
+    const result = arr.map(row => createEmployeeRecord(row))
+    return result
+}
+
+let createTimeInEvent = function(record, timeDate){
+    let [date, hour] = timeDate.split(' ')
+    record.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return record
+}
+let createTimeOutEvent = function(record, timeDate){
+    let [date, hour] = timeDate.split(' ')
+    record.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return record
+}
+let hoursWorkedOnDate = function(record, dateSought){
+    const timeIn = record.timeInEvents.find(e => e.date === dateSought)
+    const timeOut = record.timeOutEvents.find(e => e.date === dateSought)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+let wagesEarnedOnDate = function(record, dateSought){
+    const basicWage = hoursWorkedOnDate(record, dateSought) * record.payPerHour
+    return basicWage
+}
+let allWagesFor = function(employee){
+    let eligibleDates = employee.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate(employee, d)
+    }, 0)
+
+    return payable
+}
+
+
+function calculatePayroll(arrayOfEmployees){
+    return arrayOfEmployees.reduce(function(bank, employee){
+        return bank + allWagesFor(employee)
+    }, 0)
+}
+
+
+function findEmployeeByFirstName(records, name){
+    return records.find(record => record.firstName === name)
+}
